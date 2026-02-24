55107527f421827a806b2fdb527f7681f0e91a3b
mbunwe-victor <mbunwevicki@gmail.com>
2026-02-24 15:22:26 +0100

    fix failed ci

--- src/main/java/com/adorsys/keycloakstatuslist/StatusListProtocolMapper.java
+++ src/main/java/com/adorsys/keycloakstatuslist/StatusListProtocolMapper.java
@@ -33,7 +33,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
